17e5d14f1428432cda6ddc60669cdb547d6c88b5
MrCeleryman <danielisbread@gmail.com>
2017-07-30 00:23:33 +1000

    normailzer

--- app/pages/vr/index.js
+++ app/pages/vr/index.js
@@ -4,20 +4,36 @@ const start = async () => {
     try {
         const data = await fetch("http://greensland.space/parkItems?lat=-27.38142559&lng=153.040722");
         const json = await data.json();
-        console.log(json.slice(0,100));
-        list = json.slice(0,100).map((item) => ({
+        list = json.slice(0, 100).map((item) => ({
             lat: item.latitude,
             lng: item.longitude
         }))
 
-    } catch(err) {
+    } catch (err) {
         console.log(err);
     }
 
-    const normalList = list.map((item) => `${Math.sin(item.lat)*Math.sin(item.lng) * 10} 0 ${Math.sin(item.lat)*Math.cos(item.lng) * 10}`);
+    const sortedLat = list.sort((a, b) => {
+        return a.lat - b.lat
+    });
+
+    const sortedLng = list.sort((a, b) => {
+        return a.lng - b.lng
+    });
+
+    const normalList = sortedLat.map((item) => {
+        return {
+            lat: (((item.lat - sortedLat[0].lat) / (sortedLat[sortedLat.length - 1].lat - sortedLat[0].lat)) - 0.5) * 10,
+            lng: (((item.lng - sortedLng[0].lng) / (sortedLat[sortedLng.length - 1].lng - sortedLng[0].lng)) - 0.5) * 10
+        };
+    })
+
+    const mappedList = normalList.map((item) => (
+        `${item.lat} 0 ${item.lng}`
+    ));
 
     const scene = document.getElementById("scene");
-    for(let item of normalList) {
+    for (let item of mappedList) {
         const box = document.createElement("a-box");
         box.setAttribute("position", item);
         box.setAttribute("rotation", "0 45 0");
